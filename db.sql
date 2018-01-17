48e7100df2060b1de539babcbb94a3074effedaa
Alessandro Minoccheri <alessandro@studiomado.it>
2018-01-17 12:58:29 +0100

    fixed user creation

--- db.sql
+++ db.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS mydb;
 
 USE mydb;
 
-GRANT ALL on mydb.* to 'myuser'@'localhost' identified by 'mypassword';
+GRANT ALL on mydb.* to 'myuser'@'%' identified by 'mypassword' WITH GRANT OPTION;
+GRANT ALL on mydb.* to 'myuser'@'127.0.0.1' identified by 'mypassword' WITH GRANT OPTION;
+GRANT ALL on mydb.* to 'myuser'@'localhost' identified by 'mypassword' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
